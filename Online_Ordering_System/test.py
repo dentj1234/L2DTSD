f79a84035cd8aedb9370bc7822e690e164c4d85f
Jayden Dent <dentj@stu.otc.school.nz>
2025-04-10 12:44:48 +1200

    stuff

--- Online_Ordering_System/test.py
+++ Online_Ordering_System/test.py
@@ -1,13 +1,33 @@
 from flask import Flask, g
+import sqlite3
 
-DATABASE = 
+DATABASE = 'database.db'
 
+#Initialize App
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def home():
     #Home Page
-    return "Hello World!"
+
+    db = get_db()
+    cursor = db.cursor()
+    sql = 'SELECT * FROM item;'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return str(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
